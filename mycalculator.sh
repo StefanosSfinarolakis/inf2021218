c5720ca982ca70b93a520e2c52f1777b96353e97
StefanosSfinarolakis <98765691+StefanosSfinarolakis@users.noreply.github.com>
2022-12-06 15:10:19 +0200

    Update mycalculator.sh

--- mycalculator.sh
+++ mycalculator.sh
@@ -1,3 +1,8 @@
-cd /home/stephan/Documents/Codes/Java/javaCalculator/src
-java App
+echo Choose the App you want to be opened
+ls *.java
+read input
+echo Your file will open now
+wait -n
+sleep 2 &
+java input
 cd ~
