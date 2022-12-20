8e942076ed111d9f3c972b23a856004ccbe394ef
StefanosSfinarolakis <98765691+StefanosSfinarolakis@users.noreply.github.com>
2022-12-20 20:11:53 +0200

    Update App.sh

--- App.sh
+++ App.sh
@@ -1,8 +1,9 @@
 echo Choose the App you want to be opened
 ls *.java
 read input
-echo Your file will open now
+echo Your file will be compiled now
 wait -n
 sleep 2 &
+javac $input.java
+echo Your file will open now
 java $input
-cd ~
